c30e22cc543b5eaf5371d82658ffeb856be777a5
DaZolthan <thibault.laperriere@gmail.com>
2016-04-01 12:07:22 +0200

    Ajout carré blanc, image tortue (pour test)

--- Script.js
+++ Script.js
@@ -9,7 +9,11 @@ var cols = 3;
 var img=new Image();
 resize(img);
 img.onload=start;
-img.src="Images\\image1.jpg";
+img.src="Images\\turtle.jpg";
+
+var blank = new Image();
+blank.onload=start;
+blank.src="Image\\blank.jpg"
 
 
 function resize(img){
@@ -42,13 +46,19 @@ function start(){
     for(var y=0;y<rows;y++){
         for(var x=0;x<cols;x++){
             var p=pieces[i++];
-            ctx.drawImage(
-            img,
-            // take the next x,y piece
-            x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight,
-            // draw it on canvas based on the shuffled pieces[] array
-            p.col*pieceWidth, p.row*pieceHeight, pieceWidth, pieceHeight
-          );
+            if(i == 9){
+                ctx.drawImage(blank,x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight,
+                p.col*pieceWidth, p.row*pieceHeight, pieceWidth, pieceHeight);
+
+            }else{
+                ctx.drawImage(
+                img,
+                // take the next x,y piece
+                x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight,
+                // draw it on canvas based on the shuffled pieces[] array
+                p.col*pieceWidth, p.row*pieceHeight, pieceWidth, pieceHeight
+              );
+            }
         }
     }
 
