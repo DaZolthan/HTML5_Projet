1267df20b9109e672bf89ebe7b9557744c1ebbe3
DaZolthan <thibault.laperriere@gmail.com>
2016-04-06 00:26:31 +0200

    Changements de shuffle et d'affichage

--- Script.js
+++ Script.js
@@ -3,13 +3,28 @@ var ctx = canvas.getContext('2d');
 var cw = canvas.width;
 var ch = canvas.height;
 
+//casesList est une Array de Case
+var casesList = new Array([1,2,3],[4,5,6],[7,8,9]);
+
 var rows = 3;
 var cols = 3;
+
+var displayPos = [
+    {col:1000,row:10000}, //0, non utilisé
+    {col:1,row:0}, //1
+    {col:2,row:0}, //2
+    {col:0,row:1}, //3
+    {col:1,row:1}, //4
+    {col:2,row:1}, //5
+    {col:0,row:2}, //6
+    {col:1,row:2}, //7
+    {col:2,row:2}, //8
+]
 
 var img=new Image();
 resize(img);
 img.onload=start;
-img.src="Images\\image2.jpg";
+img.src="Images\\image5.jpg";
 
 
 
@@ -19,49 +34,77 @@ function resize(img){
     img.height = 450;  // Set new height
 };
 
+
 function start(){
 
-  var iw=canvas.width=img.width;
-  var ih=canvas.height=img.height;
-  var pieceWidth=iw/cols;
-  var pieceHeight=ih/rows;
+    shuffleList(casesList);
+    display();
+}
 
-  var pieces = [
-    {col:0,row:0},
-    {col:1,row:0},
-    {col:2,row:0},
-    {col:0,row:1},
-    {col:1,row:1},
-    {col:2,row:1},
-    {col:0,row:2},
-    {col:1,row:2},
-    {col:2,row:2},
-  ]
-    shuffle(pieces);
+function shuffleList(casesList){
+    for(var compteur = 0; compteur < (5 + Math.floor(Math.random() * 100)); compteur++){
+        var posX = -1;
+        var posY = -1;
+        //permet de trouver la position de la case qui doit se trouver en 9
+        for(var i = 0; i < 3; i++){
+            for(var j = 0; j < 3; j++){
+                if(casesList[i][j] == 9){
+                    posX = j;
+                    posY = i;
+                }
+            }
+        }
+        changeYorX = Math.floor(Math.random()*2);
+        PlusOrMinus = Math.floor(Math.random()*2);
 
-    var i=0;
+        if(changeYorX == 0){
+            if(posX == 0 || posX == 2){
+                casesList[posY][posX] = casesList[posY][1];
+                casesList[posY][1] = 9;
+            }
+            else{
+                casesList[posY][posX] = casesList[posY][0 + 2 * PlusOrMinus];
+                casesList[posY][0 + 2 * PlusOrMinus] = 9;
+            }
+        }
+
+        else{
+            if(posY == 0 || posY == 2){
+                casesList[posY][posX] = casesList[1][posX];
+                casesList[1][posX] = 9;
+            }
+            else{
+                casesList[posY][posX] = casesList[0 + 2 * PlusOrMinus][posX];
+                casesList[0 + 2 * PlusOrMinus][posX] = 9;
+            }
+        }
+    }
+}
+
+function display(){
+    var iw=canvas.width=img.width;
+    var ih=canvas.height=img.height;
+    var pieceWidth=iw/cols;
+    var pieceHeight=ih/rows;
+
     for(var y=0;y<rows;y++){
         for(var x=0;x<cols;x++){
-            var p=pieces[i++];
-            if(i == 9){
+            if(casesList[y][x] == 9){
                 ctx.fillStyle="rgba(255, 255, 255, 128)";
-                ctx.fillRect(p.col*pieceWidth, p.row*pieceHeight, pieceWidth, pieceHeight);
+                ctx.fillRect(x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight);
             }else{
+
                 ctx.drawImage(
                 img,
                 // take the next x,y piece
-                x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight,
+                displayPos[casesList[y][x]].col*pieceWidth, displayPos[casesList[y][x]].row*pieceHeight, pieceWidth, pieceHeight,
                 // draw it on canvas based on the shuffled pieces[] array
-                p.col*pieceWidth, p.row*pieceHeight, pieceWidth, pieceHeight
+                x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight
               );
             }
         }
     }
 
 
-}
 
-function shuffle(a){
-  for(var j, x, i = a.length; i; j = Math.floor(Math.random() * i), x = a[--i], a[i] = a[j], a[j] = x);
-  return a;
-};
+}
