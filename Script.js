5c0d31f1f503ec2dd17ec9cafa63600fc724447c
DaZolthan <thibault.laperriere@gmail.com>
2016-04-06 01:35:43 +0200

    Jeu fonctionnel, overdose de café.

--- Script.js
+++ Script.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById("canvasContainer");
 var ctx = canvas.getContext('2d');
 var cw = canvas.width;
 var ch = canvas.height;
+canvas.addEventListener("mousedown", clickHandler);
 
 //casesList est une Array de Case
 var casesList = new Array([1,2,3],[4,5,6],[7,8,9]);
@@ -10,7 +11,7 @@ var rows = 3;
 var cols = 3;
 
 var displayPos = [
-    {col:1000,row:10000}, //0, non utilisé
+    {col:0,row:0}, //0
     {col:1,row:0}, //1
     {col:2,row:0}, //2
     {col:0,row:1}, //3
@@ -24,7 +25,7 @@ var displayPos = [
 var img=new Image();
 resize(img);
 img.onload=start;
-img.src="Images\\image5.jpg";
+img.src="Images\\image4.jpg";
 
 
 
@@ -36,9 +37,9 @@ function resize(img){
 
 
 function start(){
-
     shuffleList(casesList);
     display();
+    victoryCheck();
 }
 
 function shuffleList(casesList){
@@ -92,12 +93,15 @@ function display(){
             if(casesList[y][x] == 9){
                 ctx.fillStyle="rgba(255, 255, 255, 128)";
                 ctx.fillRect(x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight);
+                ctx.strokeStyle = "#000";
+                ctx.lineWidth = 1;
+                ctx.strokeRect(x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight);
             }else{
 
                 ctx.drawImage(
                 img,
                 // take the next x,y piece
-                displayPos[casesList[y][x]].col*pieceWidth, displayPos[casesList[y][x]].row*pieceHeight, pieceWidth, pieceHeight,
+                displayPos[casesList[y][x] - 1].col*pieceWidth, displayPos[casesList[y][x] - 1].row*pieceHeight, pieceWidth, pieceHeight,
                 // draw it on canvas based on the shuffled pieces[] array
                 x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight
               );
@@ -108,3 +112,80 @@ function display(){
 
 
 }
+
+function clickHandler(event){
+    if (event.x != undefined && event.y != undefined){
+          x = event.x;
+          y = event.y;
+    }
+    else{
+      x = event.clientX + document.body.scrollLeft +
+          document.documentElement.scrollLeft;
+      y = event.clientY + document.body.scrollTop +
+          document.documentElement.scrollTop;
+    }
+
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+
+    indexX = Math.trunc(x/150);
+    indexY = Math.trunc(y/150);
+
+    moveCases(indexX,indexY);
+
+    victoryCheck();
+}
+
+function moveCases(x, y){
+    var posX = -1;
+    var posY = -1;
+    //permet de trouver la position de la case qui doit se trouver en 9
+    for(var i = 0; i < 3; i++){
+        for(var j = 0; j < 3; j++){
+            if(casesList[i][j] == 9){
+                posX = j;
+                posY = i;
+            }
+        }
+    }
+    if((x - posX == 1) && (x >= 1) && (y == posY)){
+        casesList[posY][posX] = casesList[y][x];
+        casesList[y][x] = 9;
+    }
+
+    if((posX - x == 1) && (x <= 1) && (y == posY)){
+        casesList[posY][posX] = casesList[y][x];
+        casesList[y][x] = 9;
+    }
+
+    if((y - posY == 1) && (y >= 1) && (x == posX)){
+        casesList[posY][posX] = casesList[y][x];
+        casesList[y][x] = 9;
+    }
+
+    if((posY - y == 1) && (y <= 1) && (x == posX)){
+        casesList[posY][posX] = casesList[y][x];
+        casesList[y][x] = 9;
+    }
+
+    display();
+}
+
+function victoryCheck(){
+    varTest = 1;
+    gameIsWon = true;
+    for(var i = 0; i < 3; i++){
+        for(var j = 0; j < 3; j++){
+            if(casesList[i][j] != varTest){
+                gameIsWon = false;
+            }
+            varTest++;
+        }
+    }
+    if(gameIsWon == true){
+        alert("BRAVO ! VOUS AVEZ REUSSI !");
+        canvas.removeEventListener("mousedown", moveCases);
+
+    }
+}
+
